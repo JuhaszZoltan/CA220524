1ee3d8d8a48a9527cb7135a2041f5c7525fdd71f
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2022-05-24 14:38:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Szia! Hogy hívnak?");
+string nev = Console.ReadLine();
+Console.Clear();
+Console.WriteLine($"Szia {nev}! Nagyon szép neved van! <3");
+Console.WriteLine("Nézd!");
+Random rnd = new();
+for (int i = 0; i < 100; i++)
+{
+    Console.SetCursorPosition(
+        rnd.Next(Console.WindowWidth),
+        rnd.Next(2, Console.WindowHeight));
+
+    Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+    Console.WriteLine(nev);
+
+    Thread.Sleep(100);
+}
+Console.ReadKey(true);
